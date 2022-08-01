91af6d8ec901a90bb2e92e157a995fb16f8c2917
tttKiet <90326899+tttKiet@users.noreply.github.com>
2022-08-01 14:25:35 +0700

    106. Xây dựng Logic phần Header #3

--- src/Components/Layout/components/Search/index.js
+++ src/Components/Layout/components/Search/index.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import AccountItem from '~/Components/AccountItem';
 import styles from './Search.module.scss';
 import { useState, useEffect, useRef } from 'react';
+import { useDebounce } from '~/Hooks';
 
 const cx = classNames.bind(styles);
 
@@ -14,18 +15,19 @@ function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
+    const debounced = useDebounce(searchValue, 500);
 
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -34,7 +36,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
 
     const handleHideResult = () => {
         setShowResult(false);
@@ -48,7 +50,6 @@ function Search() {
                 <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                     <PopperWrapper>
                         <h4 className={cx('search-title')}>Accounts</h4>
-                        {console.log(searchResult)}
                         {searchResult.map((result) => (
                             <AccountItem key={result.id} data={result} />
                         ))}
