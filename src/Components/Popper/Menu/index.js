6a2b665586a41e9e0c5f5c25f9a908b84a385141
tttKiet <90326899+tttKiet@users.noreply.github.com>
2022-08-05 20:49:47 +0700

    End Header but dont edit more

--- src/Components/Popper/Menu/index.js
+++ src/Components/Popper/Menu/index.js
@@ -32,6 +32,14 @@ function Menu({ children, items = [], onChange = () => {} }) {
         });
     }
 
+    const handleResetMenu = () => {
+        setHistory((prev) => prev.slice(0, 1));
+    };
+
+    const handleBack = () => {
+        setHistory((prev) => prev.slice(0, prev.length - 1));
+    };
+
     return (
         <Tippy
             offset={[12, 8]}
@@ -41,19 +49,12 @@ function Menu({ children, items = [], onChange = () => {} }) {
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
                     <PopperWrapper className={cx('menu-popper')}>
-                        {history.length > 1 && (
-                            <Header
-                                title={current.title}
-                                onBack={() => {
-                                    setHistory((prev) => prev.slice(0, prev.length - 1));
-                                }}
-                            />
-                        )}
+                        {history.length > 1 && <Header title={current.title} onBack={handleBack} />}
                         <div className={cx('menu-body')}>{renderItems()}</div>
                     </PopperWrapper>
                 </div>
             )}
-            onHide={() => setHistory((prev) => prev.slice(0, 1))}
+            onHide={handleResetMenu}
         >
             {children}
         </Tippy>
